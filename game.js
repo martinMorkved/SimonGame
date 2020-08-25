1b7e015175eb54d985069b3cd2dddb4d59642c36
martinMorkved <38659462+martinMorkved@users.noreply.github.com>
2020-08-25 13:49:02 +0200

    Add files via upload

---
+++ game.js
@@ -0,0 +1,6 @@
+buttonColours[('red', 'blue', 'green', 'yellow')];
+
+var randomChosenColour = function nextSequence() {
+    var randomNUmber = Math.floor(Math.random() * 3);
+    return randomNUmber;
+};
